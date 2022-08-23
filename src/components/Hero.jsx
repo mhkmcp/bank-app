0ad1331dbd30f44af59c45031875f67f9d3dd1d8
Humayun Kabir <mhkmcp@yahoo.com>
2022-08-23 23:28:48 +0600

    updated hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
                 </p>
             </div>    
 
-            <div className={`flex-2 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+            <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
               <img src={robot} alt="billing" 
               className='w-[100%] h-[100%] relative z-[5]' />
               <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient' />
